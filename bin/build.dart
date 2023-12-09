ab5adb5345e54a912223c6b343ce307ce2e837a1
Noobware1 <110553648+Noobware1@users.noreply.github.com>
2023-12-09 17:12:54 +0530

    please god

--- bin/build.dart
+++ bin/build.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:archive/archive_io.dart';
-import 'package:meiyou_extensions_repo/utils/directory.dart';
 import 'package:meiyou_extenstions/meiyou_extenstions.dart';
-import 'update_index.json.dart';
+
+import 'dart:convert';
+
+import 'package:meiyou_extenstions/ok_http/ok_http.dart';
 
 void main(List<String> args) {
   final sourceFolder =
@@ -81,6 +83,12 @@ build(Directory builds, Directory folder) {
   });
 }
 
+Directory getExtractorsDirectory() => Directory(
+    '${Directory.current.path.substringBefore('meiyou_extensions_repo')}meiyou_extensions_repo\\lib\\extractors');
+
+Directory getBuildsDirectory() => Directory(
+    '${Directory.current.path.substringBefore('meiyou_extensions_repo')}meiyou_extensions_repo\\builds');
+
 String fixImports(String code) {
   final importRegex = RegExp(
       r"""import\s+['"]([^'"]*extractors[^'"]*)['"](?:\s+as\s+([a-zA-Z_]\w*))?\s*;""");
@@ -105,4 +113,68 @@ Map<String, String> getAllExtractors(String code) {
     allImports[e] = File('${extractorsDir.path}\\$e').readAsStringSync();
   }
   return allImports;
+}
+
+Future<File> updateIndexJson(Plugin plugin) async {
+  final client = OKHttpClient();
+  final indexUrl =
+      "https://raw.githubusercontent.com/Noobware1/meiyou_extensions_repo/builds/index.json";
+
+  final index = (await client.get(indexUrl)).json(IndexJson.fromJson);
+  switch (plugin.type.toLowerCase()) {
+    case 'video':
+      index.video.removeWhere((e) => e.id == plugin.id);
+      index.video.add(plugin);
+      break;
+    case 'image':
+      index.image.removeWhere((e) => e.id == plugin.id);
+      index.image.add(plugin);
+      break;
+    case 'text':
+      index.text.removeWhere((e) => e.id == plugin.id);
+      index.text.add(plugin);
+      break;
+  }
+  final file = File(
+      "${Directory.current.path.substringBefore("meiyou_extensions_repo")}meiyou_extensions_repo\\builds\\index.json");
+  await file.writeAsString(index.encode);
+  return file;
+}
+
+class IndexJson {
+  final List<Plugin> video;
+
+  final List<Plugin> image;
+  final List<Plugin> text;
+
+  IndexJson({required this.video, required this.image, required this.text});
+
+  factory IndexJson.decode(String json) => IndexJson.fromJson(jsonDecode(json));
+
+  String get encode => JsonEncoder.withIndent('    ').convert(toJson());
+
+  factory IndexJson.fromJson(dynamic json) {
+    final video = json['video'] as List? ?? [];
+
+    final image = json['image'] as List? ?? [];
+
+    final text = json['text'] as List? ?? [];
+
+    return IndexJson(
+      video: video.mapAsList((it) => Plugin.fromJson(
+          (it as Map).map((key, value) => MapEntry(key.toString(), value)))),
+      image: image.mapAsList((it) => Plugin.fromJson(
+          (it as Map).map((key, value) => MapEntry(key.toString(), value)))),
+      text: text.mapAsList((it) => Plugin.fromJson(
+          (it as Map).map((key, value) => MapEntry(key.toString(), value)))),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'video': video.mapAsList((it) => it.toJson()),
+      'image': image.mapAsList((it) => it.toJson()),
+      'text': text.mapAsList((it) => it.toJson()),
+    };
+  }
 }
